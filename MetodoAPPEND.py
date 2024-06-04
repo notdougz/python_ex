98e5cc863cf335e9b55f08943783f085fc00bdda
Douglas <denbinsk4853@gmail.com>
2024-06-04 19:15:46 -0300

    alterado parametro do código

--- MetodoAPPEND.py
+++ MetodoAPPEND.py
@@ -21,5 +21,5 @@ for indice in range(0, len(equipamentos)):
 busca = input("Digite o nome do equipamento que deseja buscar: ")
 for indice in range(0, len(equipamentos)):
     if busca == equipamentos[indice]:
-        print("Valor..: ", valores[indice])
+        print("Valor.: ", valores[indice])
         print("Serial.:", seriais[indice])
