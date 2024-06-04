18d82c47ce3dcd8769374114437263fe4b94dd59
Douglas <denbinsk4853@gmail.com>
2024-06-04 19:22:44 -0300

    adicionando tag

--- MetodoAPPEND.py
+++ MetodoAPPEND.py
@@ -22,4 +22,4 @@ busca = input("Digite o nome do equipamento que deseja buscar: ")
 for indice in range(0, len(equipamentos)):
     if busca == equipamentos[indice]:
         print("Valor.: ", valores[indice])
-        print("Serial.:", seriais[indice])
+        print("Serial..:", seriais[indice])
